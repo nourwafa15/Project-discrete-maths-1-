54b18e9ae2ad75f2ae29c0438d48f612ed046df7
nourwafa15 <121030292+nourwafa15@users.noreply.github.com>
2022-12-19 14:26:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write The Prime Number from n1 to n2");
+            Console.Write("Enter The Number1 : ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter The Number2 : ");
+            int n2 = int.Parse(Console.ReadLine());
+            for (; n1 <= n2; n1++)
+            {
+                if (n1 % 2 != 0 && n1 % 3 != 0)
+                {
+                    Console.WriteLine(n1);
+                }
+
+
+
+
+            }
+        }
+    }
+}
+    
+
